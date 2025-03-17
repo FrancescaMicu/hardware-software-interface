55787cca69303bc5351a3a1aa34b95fd44b64015
FrancescaMicu <francescamariamicu@gmail.com>
2025-03-17 17:36:15 +0200

    Rezolvare laborator 3
    Signed-off-by: FrancescaMicu <francescamariamicu@gmail.com>

--- labs/lab-03/tasks/binary_search/support/binary_search.c
+++ labs/lab-03/tasks/binary_search/support/binary_search.c
@@ -13,5 +13,23 @@ int binary_search(int *v, int len, int dest)
 	/**
 	 * TODO: Implement binary search
 	 */
-	return -1;
+	caut:
+		if ( start > end ) {
+			return -1;
+		}
+		middle = (start + end) / 2;
+		if ( v[middle] > dest) {
+			goto stanga;
+		}
+		if ( v[middle] == dest ) {
+			return middle;
+		}
+		start = middle + 1;
+		goto caut;
+
+	stanga:
+		end = middle - 1;
+		goto caut;
+	
+	// return -1;
 }
