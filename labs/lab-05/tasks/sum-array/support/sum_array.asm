4e013afa657d3c79f1b35945bcdcf295c396c662
FrancescaMicu <francescamariamicu@gmail.com>
2025-03-31 17:52:00 +0300

    Rezolvare lab 5
    Signed-off-by: FrancescaMicu <francescamariamicu@gmail.com>

--- labs/lab-05/tasks/sum-array/support/sum_array.asm
+++ labs/lab-05/tasks/sum-array/support/sum_array.asm
@@ -1,5 +1,3 @@
-; SPDX-License-Identifier: BSD-3-Clause
-
 %include "printf32.asm"
 
 %define ARRAY_SIZE    10
@@ -10,7 +8,8 @@ section .data
     dword_array dd 1392849, 12544, 879923, 8799279, 7202277, 971872, 28789292, 17897892, 12988, 8799201
     dword_array2 dd 1392, 12544, 7992, 6992, 7202, 27187, 28789, 17897, 12988, 17992 ; for squares
     big_numbers_array dd 20000001, 3000000, 3000000, 23456789, 56789123, 123456789, 987654321, 56473829, 87564836, 777777777
-    ; HINT: define two variables for the big_numbers_sum
+    inceput dd 0
+    final dd 0
 
 section .text
 extern printf
@@ -19,22 +18,66 @@ main:
     push ebp
     mov ebp, esp
 
-    mov ecx, ARRAY_SIZE     ; Use ecx as loop counter.
-    xor eax, eax            ; Use eax to store the sum.
-    xor edx, edx            ; Store current value in dl; zero entire edx.
+    mov ecx, ARRAY_SIZE  
+    xor eax, eax    
+    xor edx, edx 
 
 add_byte_array_element:
     mov dl, byte [byte_array + ecx - 1]
     add eax, edx
-    loop add_byte_array_element ; Decrement ecx, if not zero, add another element.
+    loop add_byte_array_element
+    PRINTF32 `Array sum is %u\n\x0`, eax
+
+    mov ecx, ARRAY_SIZE
+    mov eax, 0
+    mov edx, 0
+add_word_array_element:
+    mov dx, word [word_array + 2*(ecx - 1)]
+    add eax, edx
+    loop add_word_array_element
+    PRINTF32 `Array sum is %u\n\x0`, eax
+
+    mov ecx, ARRAY_SIZE
+    mov eax, 0
+    mov edx, 0
+
+add_dword_array_element:
+    mov edx, dword [dword_array + 4 * (ecx - 1)]
+    add eax, edx
+    loop add_dword_array_element
+    PRINTF32 `Array sum is %u\n\x0`, eax
+
+    mov ecx, ARRAY_SIZE
+    mov eax, 0
+    mov edx, 0
+
+add_dword_array_element_squares:
+    push eax
+    mov eax, dword [dword_array2 + 4 * (ecx - 1)]
+    mov edx, dword [dword_array2 + 4 * (ecx - 1)]
+    mul edx
+    mov edx, eax
+    pop eax
+    add eax, edx
+    loop add_dword_array_element_squares
 
     PRINTF32 `Array sum is %u\n\x0`, eax
 
-    ; TODO: Compute sum for elements in word_array and dword_array.
+    mov ecx, ARRAY_SIZE
+    mov eax, 0
+    mov edx, 0
 
-    ; TODO: Compute the sum of squares for elements in dword_array2
+add_dword_array_element_big:
+    xor edx, edx
+    mov eax, dword [big_numbers_array + 4 * (ecx - 1)]
+    mov ebx, dword [big_numbers_array + 4 * (ecx - 1)]
+    mul ebx              
+    add [inceput], eax
+    adc [final], edx 
+    loop add_dword_array_element_big
 
-    ; TODO: Compute the sum of squares for elements in big_numbers_array
+    PRINTF32 `Array sum is %lld\n\x0`, [inceput], [final]
+
 
     leave
     ret
